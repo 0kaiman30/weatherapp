d1b26256156481c8d6066a8c1f2a45327ee07ea4
kairat <imanculove@gmail.com>
2025-06-17 11:16:57 +0600

    first commit

---
+++ src/app/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside>WEATHER APP</aside>
+  <div class="container">
+    <p>Привет</p>
+  </div>
+</template>
+
+<style scoped>
+aside {
+  background-color: var(--primary-color);
+  color: var(--secondary-color);
+  padding: 20px;
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  border-radius: 10px;
+}
+
+p {
+  font-size: 40px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './app/global.scss'
+import App from './app/App.vue'
+
+createApp(App).mount('#app')
